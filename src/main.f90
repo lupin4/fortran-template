44cc7f558e47b00dfac316aaa8c740b2095288c0
David Clabaugh <davidclabaugh@fullsail.edu>
2025-09-15 22:05:30 -0700

    init commit

---
+++ src/main.f90
@@ -0,0 +1,4 @@
+program main
+  implicit none
+  print *, "Hello from Fortran template!"
+end program main
---
+++ test/test_main.f90
@@ -0,0 +1,4 @@
+program test_main
+  implicit none
+  print *, "Running tests (placeholder)..."
+end program test_main
